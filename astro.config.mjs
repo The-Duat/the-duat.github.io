89e0286c4c76caf4073626d8f4a52374c3a33c02
Tracker-Friendly <jliwin98@danwin1210.de>
2023-11-04 18:35:53 +0000

    Fixed light-mode colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://the-duat.github.io',
+	site: 'https://tracker-friendly.github.io',
 	integrations: [
 		starlight({
 			title: 'The Duat',
